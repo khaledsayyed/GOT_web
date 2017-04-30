1a48d73e96137acdecda46b9249409e267c2ae2d
kse09 <khd.sayed@gmail.com>
2017-04-30 15:07:06 +0300

    sidebar

--- characters.php
+++ characters.php
@@ -5,6 +5,8 @@
 <link href="GOT_style.css" rel="stylesheet" type="text/css"/>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css">
+  <link rel="stylesheet" href="./assets/css/normalize.css" />
+    <link rel="stylesheet" href="./assets/css/main.css" />
 <script type="text/javascript" src="jquery.js"></script>
 <script type="text/javascript" src="get_characters.js"></script>
  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
--- get_characters.js
+++ get_characters.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-
+$( "#main-sidebar" ).simplerSidebar( {
+	  selectors: {
+	    trigger: "#toggle-sidebar",
+	    quitter: ".close-sb"
+	  }
+	});
     $( "#dialog" ).dialog({
       autoOpen: false,
       show: {
