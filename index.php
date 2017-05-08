5eb0f7a83b04a339f4c134fd7efc4a1c442a3190
kse09 <khd.sayed@gmail.com>
2017-05-09 02:55:44 +0300

    pics

--- index.php
+++ index.php
@@ -65,16 +65,36 @@ $(document).ready(function(){
         $(this).children("ul").stop(true,true).fadeToggle("medium"),
         $(this).toggleClass("dropdown-active");
     });
-
-	$(".team").hover(function(){
-	$("#"+$(this).attr("id")+"3").animate({"width":"80","height":"190px","top":"240px","right":"20px"},"slow");
-	$("#"+$(this).attr("id")+"2").animate({"width":"100","height":"230px","top":"225px","right":"10px"},"slow");
+	$("#jon").hover(function(){
+	$("#"+$(this).attr("id")+"3").animate({"width":"80","height":"190px","top":"15px","right":"20px"},"slow");
+	$("#"+$(this).attr("id")+"2").animate({"width":"100","height":"230px","top":"5px","right":"10px"},"slow");
 	},function(){
-	$("#"+$(this).attr("id")+"3").animate({"width":"70","height":"170px","top":"250px","right":"25px"},"slow");
-	$("#"+$(this).attr("id")+"2").animate({"width":"70","height":"70","top":"250px","right":"25px"},"slow");
+	$("#"+$(this).attr("id")+"3").animate({"width":"70px","height":"170px","top":"25px","right":"25px"},"slow");
+	$("#"+$(this).attr("id")+"2").animate({"width":"70px","height":"170px","top":"25px","right":"25px"},"slow");
 	});
-}
-);
+	$("#dany").hover(function(){
+	$("#"+$(this).attr("id")+"3").animate({"width":"80","height":"190px","top":"15px","left":"20px"},"slow");
+	$("#"+$(this).attr("id")+"2").animate({"width":"100","height":"230px","top":"5px","left":"10px"},"slow");
+	},function(){
+	$("#"+$(this).attr("id")+"3").animate({"width":"70","height":"170px","top":"25px","left":"25px"},"slow");
+	$("#"+$(this).attr("id")+"2").animate({"width":"70","height":"170","top":"25px","left":"25px"},"slow");
+	});
+$("#finger").hover(function(){
+	$("#"+$(this).attr("id")+"3").animate({"width":"80","height":"190px","top":"290px","left":"20px"},"slow");
+	$("#"+$(this).attr("id")+"2").animate({"width":"100","height":"230px","top":"275px","left":"10px"},"slow");
+	},function(){
+	$("#"+$(this).attr("id")+"3").animate({"width":"70","height":"170px","top":"300px","left":"25px"},"slow");
+	$("#"+$(this).attr("id")+"2").animate({"width":"70","height":"170","top":"300px","left":"25px"},"slow");
+	});
+	$("#cersei").hover(function(){
+	$("#"+$(this).attr("id")+"3").animate({"width":"80","height":"190px","top":"290px","right":"20px"},"slow");
+	$("#"+$(this).attr("id")+"2").animate({"width":"100","height":"230px","top":"275px","right":"10px"},"slow");
+	},function(){
+	$("#"+$(this).attr("id")+"3").animate({"width":"70","height":"170px","top":"300px","right":"25px"},"slow");
+	$("#"+$(this).attr("id")+"2").animate({"width":"70","height":"170","top":"300px","right":"25px"},"slow");
+	});
+});
+
 	</script>
 </head>
 <body>
@@ -86,14 +106,14 @@ $(document).ready(function(){
 </video-->
 <div id="our_cool_div">
 <img id="dany" src="images/dany.jpg" class="team" title="Danerys Targarian" alt="Danerys Targarian" height="170" width="70"/>
-<img id="jon" src="images/snow.jpg" class="team" title="Jon Snow" alt="Jon Snow" height="170" width="70"/>
-<img id="cersei" src="images/cersei.jpg" class="team" title="Cersei Lannister" alt="Cersei Lannister" height="170" width="70"/>
+<img id="jon" src="images/snow.jpg" class="team_r" title="Jon Snow" alt="Jon Snow" height="170" width="70"/>
+<img id="cersei" src="images/cersei.jpg" class="team_r" title="Cersei Lannister" alt="Cersei Lannister" height="170" width="70"/>
 <img id="finger" src="images/finger.jpg" class="team" title="Little Finger" alt="Little Finger" height="170" width="70"/>
-<img id="dany2" src="images/dany2.jpg" title="Danerys Targarian" alt="Danerys Targarian" height="184" width="80"/>
+<img id="dany2" src="images/dany2.jpg" title="Danerys Targarian" alt="Danerys Targarian" height="170" width="70"/>
 <img id="jon2" src="images/snow2.jpg" title="Jon Snow" alt="Jon Snow" height="170" width="70"/>
 <img id="cersei2" src="images/cersei2.jpg"  title="Cersei Lannister" alt="Cersei Lannister" height="170" width="70"/>
 <img id="finger2" src="images/finger2.jpg"  title="Little Finger" alt="Little Finger" height="170" width="70"/>
-<img id="dany3" src="images/dany3.jpg" title="Danerys Targarian" alt="Danerys Targarian" height="184" width="80"/>
+<img id="dany3" src="images/dany3.jpg" title="Danerys Targarian" alt="Danerys Targarian" height="170" width="70"/>
 <img id="jon3" src="images/snow3.jpg" title="Jon Snow" alt="Jon Snow" height="170" width="70"/>
 <img id="finger3" src="images/finger3.jpg"  title="Little Finger" alt="Little Finger" height="170" width="70"/>
 <img id="cersei3" src="images/cersei3.jpg"  title="Cersei Lannister" alt="Cersei Lannister" height="170" width="70"/>
